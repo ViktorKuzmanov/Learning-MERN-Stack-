f86f2a6b2db34851439272c20433274e003730ab
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-17 11:54:21 +0200

    connect to mongoDB database

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,15 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+mongoose.connect(process.env.ATLAS_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("mongodb databse connection establiched succefully");
+});
+
 app.use(cors());
 app.use(express.json());
 
