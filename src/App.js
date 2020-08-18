9450ede5fb1f2e17a7c8ec5fe279a48e69e61cc1
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-18 16:02:27 +0200

    add ExercisesList component and route to it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ExercisesList from "./components/ExercisesList";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Route path="/" exact component={ExercisesList} />
     </Router>
   );
 }
