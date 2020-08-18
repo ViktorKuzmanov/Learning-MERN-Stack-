01036caa426c1856dd046fac49da7f62688fad33
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-18 16:05:43 +0200

    add EditExercises component and route to it

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ExercisesList from "./components/ExercisesList";
+import EditExercises from "./components/EditExercises";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" exact component={EditExercises} />
     </Router>
   );
 }
