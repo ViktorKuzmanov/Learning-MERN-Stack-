a56989ac04f6e85a447173f207cb442d7a19baaa
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-18 15:40:35 +0200

    wrap navbar into react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+    </Router>
   );
 }
 
