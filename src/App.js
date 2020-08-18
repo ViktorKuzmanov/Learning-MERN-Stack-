b899356589bf3edf128975782ed39c7293f0296e
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-18 15:35:06 +0200

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <Router>aa</Router>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
