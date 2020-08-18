766fddc2d80d61dc8de6bbed8974a27a8b74518e
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-18 16:12:22 +0200

    add CreateExercises component and route to it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ExercisesList from "./components/ExercisesList";
 import EditExercises from "./components/EditExercises";
+import CreateExercises from "./components/CreateExercises";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Route path="/" exact component={ExercisesList} />
       <Route path="/edit/:id" exact component={EditExercises} />
+      <Route path="/create" exact component={CreateExercises} />
     </Router>
   );
 }
---
+++ src/components/CreateExercises.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class CreateExercises extends Component {
+  render() {
+    return <p>CreateExercises component</p>;
+  }
+}
