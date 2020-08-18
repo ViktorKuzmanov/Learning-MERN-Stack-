1adbfd85f644ca11dce2fabe509fb65a393873b9
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-18 16:23:51 +0200

    make all buttons on navbar to open corresponding components in router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,24 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/#">
-          Exercise-tracker
-        </a>
+        <Link to="/">Exercise tracker</Link>
         <button className="navbar-toggler">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="/#">
-                Exercises <span className="sr-only">(current)</span>
-              </a>
+              <Link to="/">Exercises</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/#">
-                Create exercise log
-              </a>
+              <Link to="/create">Create exercise log</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/#">
-                Create user
-              </a>
+              <Link to="/user">Create user</Link>
             </li>
           </ul>
         </div>
