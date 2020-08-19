0743a92c93e90faad8e3b695e732c3c108b7ba1f
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-19 21:09:14 +0200

    add state for one exercise and users

--- src/components/CreateExercises.jsx
+++ src/components/CreateExercises.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 
 export default class CreateExercises extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: [],
+    };
+  }
   render() {
     return <p>CreateExercises component</p>;
   }
