e3cefd942ba182e241408f23bd96d28346657e83
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-22 15:41:42 +0200

    get exercises from database on componentDidMount

--- src/components/ExercisesList.jsx
+++ src/components/ExercisesList.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class ExercisesList extends Component {
   constructor(props) {
@@ -6,6 +7,15 @@ export default class ExercisesList extends Component {
     this.state = { exercises: [] };
   }
 
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/exercises")
+      .then((response) => {
+        this.setState({ exercises: response.data });
+        console.log(this.state.exercises);
+      })
+      .catch((err) => console.log(err));
+  }
   render() {
     return <p>ExercisesList component</p>;
   }
