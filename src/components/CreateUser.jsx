7fe420db27d4efebd2c0605185bc81335ba37fc9
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-22 12:53:19 +0200

    add onChange callback for username input and fix error with constructor

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -3,17 +3,29 @@ import React, { Component } from "react";
 
 export default class CreateUsers extends Component {
   constructor(props) {
+    super(props);
     this.state = {
       username: "",
     };
   }
+
+  onChangeUsername = (event) => {
+    this.setState({ username: event.target.value });
+    console.log(this.state.username);
+  };
 
   render() {
     return (
       <div>
         <p>Create new user</p>
         <form>
-          <input type="text" placeholder="username" required />
+          <input
+            type="text"
+            onChange={this.onChangeUsername}
+            placeholder="username"
+            value={this.state.username}
+            required
+          />
           <button type="submit">Create user</button>
         </form>
       </div>
