fac2c1b19a3e2f8dda509e04191262b8354cf6cf
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-22 15:38:08 +0200

    add constructor and state

--- src/components/ExercisesList.jsx
+++ src/components/ExercisesList.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 
 export default class ExercisesList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { exercises: [] };
+  }
+
   render() {
     return <p>ExercisesList component</p>;
   }
