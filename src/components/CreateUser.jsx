1108047a8d65cae4cea0c7c4b58b3f9a0c972737
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-22 13:13:09 +0200

    send the user as post request to server with axios

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class CreateUsers extends Component {
   constructor(props) {
@@ -19,6 +20,9 @@ export default class CreateUsers extends Component {
     const user = {
       username: this.state.username,
     };
+    axios
+      .post("http://localhost:5000/users/add", user)
+      .then((res) => console.log(res.data));
     this.setState({ username: "" });
   };
 
