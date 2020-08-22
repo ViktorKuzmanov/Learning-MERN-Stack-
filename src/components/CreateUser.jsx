502c63a41499cd1d0a3837d7476929cbcc3b2548
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-22 12:42:36 +0200

    add state for a user

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,6 +1,13 @@
+// TODO: Implement this component
 import React, { Component } from "react";
 
 export default class CreateUsers extends Component {
+  constructor(props) {
+    this.state = {
+      username: "",
+    };
+  }
+
   render() {
     return <p>CreateUsers component</p>;
   }
